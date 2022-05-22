49fabcb8d74c515be1b571d2369918cb045913e9
Gondrong <63855592+gondrongafri@users.noreply.github.com>
2022-05-22 16:16:58 +0700

    function.php

--- tri_req.php
+++ tri_req.php
@@ -1,106 +1,98 @@
 <?php
+function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
+$header[] = "Host: api.gojekapi.com";
+$header[] = "User-Agent: okhttp/3.10.0";
+$header[] = "Accept: application/json";
+$header[] = "Accept-Language: id-ID";
+$header[] = "Content-Type: application/json; charset=UTF-8";
+$header[] = "X-AppVersion: 3.46.1";
+$header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
+$header[] = "Connection: keep-alive";
+$header[] = "X-User-Locale: id_ID";
+$header[] = "X-Location: -6.220".mt_rand(100,999).",106.77".mt_rand(1000,9999);
+if ($pin):
+$header[] = "pin: $pin";
+    endif;
+if ($token):
+$header[] = "Authorization: Bearer $token";
+endif;
+if ($otpsetpin):
+$header[] = "otp: $otpsetpin";
+endif;
+if ($uuid):
+$header[] = "User-uuid: $uuid";
+endif;
+$c = curl_init("https://api.gojekapi.com".$url);
+    curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    if ($data):
+    curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($c, CURLOPT_POST, true);
+    endif;
+    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($c, CURLOPT_HEADER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, $header);
+    $response = curl_exec($c);
+    $httpcode = curl_getinfo($c);
+    if (!$httpcode)
+        return false;
+    else {
+        $header = substr($response, 0, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+        $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+    }
+    $json = json_decode($body, true);
+    return $body;
+}
+function save($filename, $content)
+{
+    $save = fopen($filename, "a");
+    fputs($save, "$content\r\n");
+    fclose($save);
+}
+function nama()
+    {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $ex = curl_exec($ch);
+    // $rand = json_decode($rnd_get, true);
+    preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
+    return $name[2][mt_rand(0, 14) ];
+    }
+function getStr($a,$b,$c){
+	$a = @explode($a,$c)[1];
+	return @explode($b,$a)[0];
+}
+function getStr1($a,$b,$c,$d){
+        $a = @explode($a,$c)[$d];
+        return @explode($b,$a)[0];
+}
 
-class tri{
-  
-  function curls($host,$header,$body,$method)
-  	{
-  		$ch = curl_init();
-  		curl_setopt($ch, CURLOPT_URL, $host);
-  		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);	
-  		curl_setopt($ch, CURLOPT_HEADER, true);
-  		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-  		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  		curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
-  		curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-  		curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-  		curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
-  		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-  		$req = curl_exec($ch);
-  		$req = explode("\r\n\r\n", $req);
-  		return $req;
-  	}
-
-
-
-  function request_otp($msisdn,$imei)
-  {
-    $body = array("msisdn"=>$msisdn);
-    $body = json_encode($body);
-    $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:" . $ctl ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/login?returnUrl=%2Fhome" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-    $response = $this->curls('http://bonstri.tri.co.id/api/v1/login/request-otp',$header,$body,'POST');
-    return $response;
-  }
-  
-  function login($msisdn,$otp)
-  {
-    $body = "grant_type".'='."password".'&'."username".'=' . $msisdn . '&'."password".'='.$otp;
-    $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:" . $ctl ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "Authorization:Basic Ym9uc3RyaTpib25zdHJpc2VjcmV0" ,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/x-www-form-urlencoded" ,
- "Referer:http://bonstri.tri.co.id/login?returnUrl=%2Fhome" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-    $response = $this->curls('http://bonstri.tri.co.id/api/v1/login/validate-otp',$header,$body,'POST');
-    return $response[1];
-  }
-  
-  function trans($bearer)
-  {
-    $body = '{}';
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:2" ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "Authorization:Bearer " . $bearer,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/voucherku" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-   $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/voucher-history",$header,$body,"POST");
-   return $response[1];
-   
-  }
-  
-  function claim($bearer,$id,$id1)
-  {
-    $body = array("rewardId"=>$id1,"rewardTransactionId"=>$id);
-    $body = json_encode($body);
-    $ctl = strlen($body);
-    $header = array("Host:bonstri.tri.co.id" ,
- "Connection:keep-alive" ,
- "Content-Length:" . $ctl ,
- "Accept:application/json, text/plain, */*" ,
- "Origin:http://bonstri.tri.co.id" ,
- "Authorization:Bearer " . $bearer ,
- "User-Agent:Mozilla/5.0 (Linux; Android 9; Redmi Note 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" ,
- "Content-Type:application/json" ,
- "Referer:http://bonstri.tri.co.id/voucherku" ,
- "Accept-Encoding:gzip, deflate" ,
- "Accept-Language:id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
-     $response = $this->curls("http://bonstri.tri.co.id/api/v1/voucherku/get-voucher-code",$header,$body,"POST");
-     return $response[1];
-  }
-  
-  
-  
+function color($color = "default" , $text)
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
+function fetch_value($str,$find_start,$find_end) {
+	$start = @strpos($str,$find_start);
+	if ($start === false) {
+		return "";
+	}
+	$length = strlen($find_start);
+	$end    = strpos(substr($str,$start +$length),$find_end);
+	return trim(substr($str,$start +$length,$end));
 }
 ?>
